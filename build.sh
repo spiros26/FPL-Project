ad0d9e64fbe3079ea8da8ede00d7d65efcc9be7f
spiros26 <spvalouxis@gmail.com>
2023-09-25 00:27:23 -0400

    build script change

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 wget https://www.coin-or.org/download/binary/CoinAll/COIN-OR-1.7.4-linux-x86_64-gcc4.7.2-static.tar.gz
 tar -zxvf COIN-OR-1.7.4-linux-x86_64-gcc4.7.2-static.tar.gz
 
-export PATH="${PATH}:/opt/render/project/.render/bin/
+export PATH="${PATH}:/opt/render/project/.render/bin/"
 
 pip install -r requirements.txt
