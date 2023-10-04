02ef5e2c7a6e16c08b52c8af94fe89eef6f879ab
spiros26 <spvalouxis@gmail.com>
2023-10-04 00:35:35 -0400

    build test

--- solver_fpl/src/multi_period_dev.py
+++ solver_fpl/src/multi_period_dev.py
@@ -471,6 +471,7 @@ def solve_multi_period_fpl(data, options):
 
             else:
                 f = open(f'tmp/{problem_name}_{problem_id}_{iter}.mps', 'r')
+                command = 'cbc'
                 command = f'cbc tmp/{problem_name}_{problem_id}_{iter}.mps cost column ratio 1 solve solu tmp/{problem_name}_{problem_id}_{iter}_sol_init.txt'
                 if use_cmd:
                     os.system(command)
